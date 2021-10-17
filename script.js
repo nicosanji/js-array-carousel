14d2947d64f6113b1b01162b23d6697140ddc487
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-17 18:25:59 +0200

    creato in js funzionamento contenitore centrale con testi

--- script.js
+++ script.js
@@ -26,20 +26,60 @@ const text = [
 ];
 
 
+
 // DICHIARO & COLLEGO variabile al "contenitore HTML" dell'immagine grande 
 const myBigImg = document.querySelector(".my-big-img");
 // mostra in console
 console.log(myBigImg);
 
 // DICHIARO & COLLEGO variabile al "contenitore HTML" dei testi dell'immagine grande 
-const myBigImgTitle = document.querySelector(".my-big-img-title")
+const myBigImgTitle = document.querySelector(".my-big-img-title");
 // mostra in console
 console.log(myBigImgTitle);
 
 // DICHIARO & COLLEGO variabile al "contenitore HTML" della lista delle immagini piccole 
-const myListImg = document.querySelector(".my-list-img")
+const myListImg = document.querySelector(".my-list-img");
 // mostra in console
 console.log(myListImg);
+
+
+
+// DICHIARO & CREO variabile iniziale a 0=(1° elemento ordine array) che indicherà "dove sono ora" -> (poi cambierà)
+let nowSelected = 0;
+
+
+// DICHIARO & CREO variabile "vuota" per il ciclo
+let i = "";
+
+// CREO IL CICLO FOR per inserire/scorrere gli array per la colonna di sinistra con img + testo nell'HTML 
+for (let i = 0; i < items.length; i++) {
+     
+    const nowImg = items[i];
+
+    let initial = "";
+
+    if (i === nowSelected) {
+
+        initial = "active";
+
+    }
+    // CREO la variabile chè è anche il tag img ora selezionato dall'array che è stato "ciclato"
+    const imgSelected = `<img class="${initial}" src="${nowImg}" alt="">`;
+    // aggiungo ad HTML 
+    myBigImg.innerHTML += imgSelected;
+
+    // CREO la variabile chè è anche il testo ora selezionato dall'array che è stato "ciclato"
+    const textSelected = `<div class="${initial} container-text"><h4>${title[i]}</h4>${text[i]}</div>`;
+    // aggiungo ad HTML
+    document.querySelector(".my-big-img-title").innerHTML += textSelected;
+}
+
+
+
+// DICHIARO & COLLEGO variabile al "HTML" della freccia in alto (up)
+const iconBoxUp = document.querySelector(".icon-box-up");
+// DICHIARO & COLLEGO variabile al "HTML" della freccia in basso (down)
+const iconBoxDown = document.querySelector(".icon-box-down");
 
 
 
