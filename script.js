f148ed49a567eef8d2939eb8d34825e933d1fb0b
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-17 17:31:50 +0200

    iniziato JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-const titoloPrincipale = document.getElementById("titolo_principale");
+// DICHIARO/CREO array "items" = immagini
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+// DICHIARO/CREO array "title" = titoli
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+// DICHIARO/CREO array "text" = paragrafi/sottotitoli
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+
+// DICHIARO & COLLEGO variabile al "contenitore HTML" dell'immagine grande 
+const myBigImg = document.querySelector(".my-big-img");
+// mostra in console
+console.log(myBigImg);
+
+// DICHIARO & COLLEGO variabile al "contenitore HTML" dei testi dell'immagine grande 
+const myBigImgTitle = document.querySelector(".my-big-img-title")
+// mostra in console
+console.log(myBigImgTitle);
+
+// DICHIARO & COLLEGO variabile al "contenitore HTML" della lista delle immagini piccole 
+const myListImg = document.querySelector(".my-list-img")
+// mostra in console
+console.log(myListImg);
+
+
 
 
 
